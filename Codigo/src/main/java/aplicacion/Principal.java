6c7e4c89bce68d7838b3a49698b463e165b85ea9
jyustrod <jyustrod@myuax.com>
2025-02-26 18:06:04 +0100

    Main y Test Arreglados

--- Codigo/src/main/java/aplicacion/Principal.java
+++ Codigo/src/main/java/aplicacion/Principal.java
@@ -16,37 +16,58 @@ package aplicacion;
 
 import mates.Matematicas;
 import dominio.Tablero;
+
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 import java.lang.InterruptedException;
 
 public class Principal {
-    public static void main(String[] args){
-        System.out.println("El número PI es " + Matematicas.generarNumeroPi(Integer.parseInt(args[0])));
+    public static void main(String[] args) {
 
-        try {
-            Tablero tablero = new Tablero();
-            System.out.println("SIMULACIÓN CON TABLERO LEÍDO");
-            tablero.leerEstadoActual();
-            System.out.println(tablero);
+        Scanner sc = new Scanner(System.in);
+        int opcion;
 
-            for (int i = 0; i <= 5; i++) {
-                TimeUnit.SECONDS.sleep(1);
-                tablero.transitarAlEstadoSiguiente();
-                System.out.println(tablero);
+        do {
+            System.out.println("1: Generar Numero Pi\n" +
+                    "2: Juego de la Vida\n" +
+                    "3: Salir\n" +
+                    "Escoje una opcion: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("El número PI es " + Matematicas.generarNumeroPi(Integer.parseInt(args[0])));
+                    break;
+                case 2:
+                    try {
+                        Tablero tablero = new Tablero();
+                        System.out.println("SIMULACIÓN CON TABLERO LEÍDO");
+                        tablero.leerEstadoActual();
+                        System.out.println(tablero);
+
+                        for (int i = 0; i <= 5; i++) {
+                            TimeUnit.SECONDS.sleep(1);
+                            tablero.transitarAlEstadoSiguiente();
+                            System.out.println(tablero);
+                        }
+
+                        System.out.println("SIMULACIÓN CON TABLERO GENERADO MEDIANTE MONTECARLO");
+                        tablero.generarEstadoActualPorMontecarlo();
+                        System.out.println(tablero);
+
+                        for (int i = 0; i <= 15; i++) {
+                            TimeUnit.SECONDS.sleep(1);
+                            tablero.transitarAlEstadoSiguiente();
+                            System.out.println(tablero);
+                        }
+
+                    } catch (InterruptedException e) {
+                        System.out.println(e);
+                    }
             }
-
-            System.out.println("SIMULACIÓN CON TABLERO GENERADO MEDIANTE MONTECARLO");
-            tablero.generarEstadoActualPorMontecarlo();
-            System.out.println(tablero);
-
-            for (int i = 0; i <= 15; i++) {
-                TimeUnit.SECONDS.sleep(1);
-                tablero.transitarAlEstadoSiguiente();
-                System.out.println(tablero);
-            }
-
-        } catch(InterruptedException e) {
-            System.out.println(e);
-        }
+        } while (opcion != 3);
     }
+
+
+
 }
